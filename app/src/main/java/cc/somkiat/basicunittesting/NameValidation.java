09ebb48bf462a65b4d00cd4b72af205a4ff07f19
J <58070014@kmitl.ac.th>
2017-11-13 23:33:58 +0700

    Add NameHaveSpecialsAlphabet, NameContainNumber

--- app/src/main/java/cc/somkiat/basicunittesting/NameValidation.java
+++ app/src/main/java/cc/somkiat/basicunittesting/NameValidation.java
@@ -1,30 +1,52 @@
 package cc.somkiat.basicunittesting;
 
+import java.util.regex.Pattern;
+
 class NameValidation {
 
     private String resultMessage = "Name Validation Success";
 
     public boolean validate (String name){
-        if (nameIsEmpty(name) == false){
-            resultMessage = "Name is Empty";
+        if (isEmpty(name) == true){
+            resultMessage = "Name is empty";
         }
-        else if (nameIsNull(name) == false){
-            resultMessage = "Name is Null";
+        else if (isNull(name) == true){
+            resultMessage = "Name is null";
+        }
+        else if (isAlphabet(name) == true){
+            resultMessage = "Name is alphabet";
+        }
+        else if (isContainNumber(name)  == true) {
+            resultMessage = "Name contain number";
         }
         return true;
     }
-    public boolean nameIsEmpty(String name) {
+
+    public boolean isEmpty(String name) {
         if(name.isEmpty()){
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 
-    public boolean nameIsNull(String name) {
+    public boolean isNull(String name) {
         if(name == null){
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 
+    public boolean isAlphabet(String name) {
+        if(Pattern.matches("[a-zA-Z]+", name)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isContainNumber(String name) {
+        if (name.matches(".*\\d+.*")){
+            return true;
+        }
+        return false;
+    }
 }
--- app/src/test/java/cc/somkiat/basicunittesting/NameValidationFailTest.java
+++ app/src/test/java/cc/somkiat/basicunittesting/NameValidationFailTest.java
@@ -3,21 +3,37 @@ package cc.somkiat.basicunittesting;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 public class NameValidationFailTest {
 
     @Test
     public void NameIsEmpty(){
             NameValidation validation = new NameValidation();
-            boolean result = validation.nameIsEmpty("");
-            assertFalse("Fail!!! Name is empty.", result);
+            boolean result = validation.isEmpty("");
+            assertTrue("Fail!!! Name is empty.", result);
     }
 
     @Test
     public void NameIsNull(){
         NameValidation validation = new NameValidation();
-        boolean result = validation.nameIsNull(null);
-        assertFalse("Fail!!! Name is null.", result);
+        boolean result = validation.isNull(null);
+        assertTrue("Fail!!! Name is null.", result);
     }
+
+    @Test
+    public void NameHaveSpecialsAlphabet(){
+        NameValidation validation = new NameValidation();
+        boolean result = validation.isAlphabet("@>N.S<@");
+        assertFalse("Fail!!! Name have alphabet.", result);
+    }
+
+    @Test
+    public void NameContainNumber(){
+        NameValidation validation = new NameValidation();
+        boolean result = validation.isContainNumber("12123saii12121");
+        assertTrue("Fail!!! Name have number.", result);
+    }
+
 
 }
